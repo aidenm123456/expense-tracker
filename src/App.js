58440d0a7802813365c8f69c5a6e3dc64d947d79
Aiden Laptop <aidenmawji@gmail.com>
2022-08-27 11:46:43 -0400

    changed form elements from default uncontrolled to controlled componenets.

--- src/App.js
+++ src/App.js
@@ -111,13 +111,13 @@ function App() {
 
         <div style={{ width:'60%'}}>
           <Box component="form" sx={{'& > :not(style)': { width: '100%' }}} noValidate autoComplete="off" >
-            <TextField id="outlined-basic" label="Description" variant="outlined" onChange={handleChangeDescription}/>
+            <TextField id="outlined-basic" label="Description" variant="outlined" value={date} onChange={handleChangeDescription}/>
           </Box>
         </div>
 
         <div style={{ width:'60%'}}>
           <Box component="form" sx={{'& > :not(style)': { width: '100%' }}} noValidate autoComplete="off" >
-            <TextField id="outlined-basic" label="YYYY-MM-DD" variant="outlined" onChange={handleChangeDate}/>
+            <TextField id="outlined-basic" label="YYYY-MM-DD" variant="outlined" value={description} onChange={handleChangeDate}/>
           </Box>
         </div>
 
