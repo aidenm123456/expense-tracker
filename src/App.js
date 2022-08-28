a3fed39d518226f4e6315bc330d4a28a7e286a18
Aiden Laptop <aidenmawji@gmail.com>
2022-08-27 21:27:34 -0400

    fixed description and date's state collision + added expense amount input.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { db } from './firebase'
 import { uid } from 'uid';
 import { set, ref, onValue } from 'firebase/database';
 import { useEffect, useState } from 'react';
-import { Typography, Box, InputLabel, MenuItem, FormControl, TextField} from '@mui/material';
+import { Typography, Box, InputLabel, MenuItem, FormControl, TextField, OutlinedInput, InputAdornment} from '@mui/material';
 import Select from '@mui/material/Select';
 
 import '@fontsource/roboto/300.css';
@@ -16,6 +16,7 @@ function App() {
   const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
   const [date, setDate] = useState('');
+  const [price, setPrice] = useState('');
 
   const handleChangeCategory = ((e) => {
     setCategory(e.target.value);
@@ -27,6 +28,10 @@ function App() {
 
   const handleChangeDate = ((e) => {
     setDate(e.target.value);
+  });
+
+  const handleChangePrice = ((e) => {
+    setPrice(e.target.value);
   });
     
   
@@ -84,6 +89,19 @@ function App() {
       <div style={{display:'flex', flexDirection:'column', alignItems:'center', borderRadius:'0.4rem', width: '70vw', backgroundColor:'whitesmoke'}}>
         
         <Typography variant='body1'>Add Expenses</Typography>
+
+        <FormControl fullWidth sx={{ width: '60%' }}>
+          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            value={price}
+            onChange={handleChangePrice}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount"
+          />
+        </FormControl>
+        
+        
         
         <Box sx={{ width: '60%' }}>
           <FormControl fullWidth>
@@ -111,13 +129,13 @@ function App() {
 
         <div style={{ width:'60%'}}>
           <Box component="form" sx={{'& > :not(style)': { width: '100%' }}} noValidate autoComplete="off" >
-            <TextField id="outlined-basic" label="Description" variant="outlined" value={date} onChange={handleChangeDescription}/>
+            <TextField id="outlined-basic" label="Description" variant="outlined" value={description} onChange={handleChangeDescription}/>
           </Box>
         </div>
 
         <div style={{ width:'60%'}}>
           <Box component="form" sx={{'& > :not(style)': { width: '100%' }}} noValidate autoComplete="off" >
-            <TextField id="outlined-basic" label="YYYY-MM-DD" variant="outlined" value={description} onChange={handleChangeDate}/>
+            <TextField id="outlined-basic" label="YYYY-MM-DD" variant="outlined" value={date} onChange={handleChangeDate}/>
           </Box>
         </div>
 
