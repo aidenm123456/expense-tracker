23be36fb1ee574475cccfe3d4ce22e4b7378a049
Aiden Laptop <aidenmawji@gmail.com>
2022-08-30 19:37:42 -0400

    added margin on top/bottom of the input for description

--- src/components/DescriptionInput.js
+++ src/components/DescriptionInput.js
@@ -2,7 +2,7 @@ import { Box, TextField} from '@mui/material';
 
 const DescriptionInput = ({ descriptionValue, onUpdate }) => {
   return (
-    <div style={{ width:'60%'}}>
+    <div style={{ width:'60%', marginBottom:'1vh'}}>
       <Box component="form" sx={{'& > :not(style)': { width: '100%' }}} noValidate autoComplete="off" >
         <TextField id="outlined-basic" label="Description" variant="outlined" value={descriptionValue} onChange={onUpdate}/>
       </Box>
