6aae208562683431f0fd4a02a96e52687e84b8fd
Aiden Laptop <aidenmawji@gmail.com>
2022-08-31 09:52:41 -0400

    added 2.5vh margin between expense inputs and list

--- src/App.js
+++ src/App.js
@@ -143,7 +143,7 @@ function App() {
       </div>
 
       {/* Expense list */}
-      <div style={{display:'flex', width: '80vw', alignItems: 'center', borderRadius:'0.4rem', justifyContent:'center', backgroundColor:'whitesmoke', boxShadow: '2px 3px 10px grey'}}>
+      <div style={{display:'flex', width: '80vw', alignItems: 'center', borderRadius:'0.4rem', justifyContent:'center', backgroundColor:'whitesmoke', boxShadow: '2px 3px 10px grey', marginTop:'2.5vh'}}>
         <div style={{ width: '100%'}}>
           <ExpenseHeading />
           {expenseData !== false ? expenseData.map((expense) => {
